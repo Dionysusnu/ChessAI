511bd063342e1afd1150968a47eec3a7222147b9
Dionysusnu2 <55697642+Dionysusnu2@users.noreply.github.com>
2019-09-23 14:35:15 +0200

    Update Move.js

--- Move.js
+++ Move.js
@@ -5,18 +5,24 @@ class Move {
 		this.x = x;
 		this.y = y;
 		this.promotesTo = promotesTo;
+		this.oldx = piece.x;
+		this.oldy = piece.y;
+		this.hasMoved = piece.hasMoved;
+		this.deleted = this.piece.board.findPiece(this.x, this.y);
 	}
 
 	solvesCheck(color) {
-		const oldx = this.piece.x;
-		const oldy = this.piece.y;
-		const hadMoved = this.piece.hasMoved;
-		const deleted = this.piece.move(this.x, this.y);
+		this.piece.move(x, y, promotesTo);
 		const solves = !this.piece.board.calcCheck(color);
-		this.piece.x = oldx;
-		this.piece.y = oldy;
-		this.piece.hasMoved = hadMoved;
-		deleted && this.piece.board.pieces.set(deleted, deleted);
+		this.revert()
 		return solves;
+	}
+	
+	revert() {
+		this.piece.x = this.oldx;
+		this.piece.y = this.oldy;
+		this.piece.hasMoved = this.hasMoved;
+		const deleted = this.deleted;
+		deleted && this.piece.board.pieces.set(deleted, deleted);
 	}
 }
